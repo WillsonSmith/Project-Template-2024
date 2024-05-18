50053433c05cd0c6681393e4ce3f3c7501343c82
Willson Smith <me@willsonsmith.com>
2024-05-17 21:52:11 -0400

    idk cache control for some reason

--- app/server/api/cats.ts
+++ app/server/api/cats.ts
@@ -4,18 +4,24 @@ const app = new Hono();
 
 app.get('/', (c) => {
   const count = Number(c.req.query('count')) || 1;
-  return c.json(
-    Array.from({ length: count }, () => {
-      const width = Math.round(Math.random() * 100 + 400);
-      const height = Math.round(Math.random() * 100 + 300);
 
-      return {
-        url: `https://loremflickr.com/${width}/${height}/cats`,
-        width,
-        height,
-      };
-    }),
-  );
+  const cats = Array.from({ length: count }, () => {
+    const width = Math.round(Math.random() * 100 + 400);
+    const height = Math.round(Math.random() * 100 + 300);
+
+    return {
+      url: `https://loremflickr.com/${width}/${height}/cats`,
+      width,
+      height,
+    };
+  });
+
+  return new Response(JSON.stringify(cats), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'max-age=300',
+    },
+  });
 });
 
 export default app;
