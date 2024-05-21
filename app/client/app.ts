2bb4a95e188bd663558634b2d76a8274bfbc4818
Willson Smith <me@willsonsmith.com>
2024-05-21 18:39:42 -0400

    init

--- app/client/app.ts
+++ app/client/app.ts
@@ -6,17 +6,16 @@ import '@webcomponents/scoped-custom-element-registry';
 
 import '@/app/app-entry';
 
-function init() {
-  const App = () => {
-    return html`<app-entry></app-entry>`;
-  };
-
-  render(App(), document.getElementById('app')!);
-}
-
 // @ts-expect-error: Property 'UrlPattern' does not exist
 if (!globalThis.URLPattern) {
   import('urlpattern-polyfill').then(() => {
     init();
   });
 } else init();
+
+function init() {
+  const App = () => {
+    return html`<app-entry></app-entry>`;
+  };
+  render(App(), document.getElementById('app')!);
+}
